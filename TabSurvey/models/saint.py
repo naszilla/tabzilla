66794583c1a985d856461328ed184fa58f4a184c
duncan <duncan.mcelfresh@gmail.com>
2022-08-30 17:31:06 -0700

    don't save model, and reformat

--- TabSurvey/models/saint.py
+++ TabSurvey/models/saint.py
@@ -1,30 +1,27 @@
 # The SAINT model.
-from models.basemodel_torch import BaseModelTorch
-
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from einops import rearrange
+from torch import einsum
 from torch.utils.data import DataLoader
 
-import numpy as np
-from torch import einsum
-from einops import rearrange
-
-from models.saint_lib.models.pretrainmodel import SAINT as SAINTModel
-from models.saint_lib.data_openml import DataSetCatCon
+from models.basemodel_torch import BaseModelTorch
 from models.saint_lib.augmentations import embed_data_mask
+from models.saint_lib.data_openml import DataSetCatCon
+from models.saint_lib.models.pretrainmodel import SAINT as SAINTModel
 
-'''
+"""
     SAINT: Improved Neural Networks for Tabular Data via Row Attention and Contrastive Pre-Training
     (https://arxiv.org/abs/2106.01342)
     
     Code adapted from: https://github.com/somepago/saint
-'''
+"""
 
 
 class SAINT(BaseModelTorch):
-
     def __init__(self, params, args):
         super().__init__(params, args)
         if args.cat_idx:
@@ -54,18 +51,22 @@ class SAINT(BaseModelTorch):
             cont_embeddings="MLP",
             attentiontype="colrow",
             final_mlp_style="sep",
-            y_dim=args.num_classes
+            y_dim=args.num_classes,
         )
 
         if self.args.data_parallel:
-            self.model.transformer = nn.DataParallel(self.model.transformer, device_ids=self.args.gpu_ids)
-            self.model.mlpfory = nn.DataParallel(self.model.mlpfory, device_ids=self.args.gpu_ids)
+            self.model.transformer = nn.DataParallel(
+                self.model.transformer, device_ids=self.args.gpu_ids
+            )
+            self.model.mlpfory = nn.DataParallel(
+                self.model.mlpfory, device_ids=self.args.gpu_ids
+            )
 
     def fit(self, X, y, X_val=None, y_val=None):
 
-        if self.args.objective == 'binary':
+        if self.args.objective == "binary":
             criterion = nn.BCEWithLogitsLoss()
-        elif self.args.objective == 'classification':
+        elif self.args.objective == "classification":
             criterion = nn.CrossEntropyLoss()
         else:
             criterion = nn.MSELoss()
@@ -75,16 +76,20 @@ class SAINT(BaseModelTorch):
         self.model.to(self.device)
 
         # SAINT wants it like this...
-        X = {'data': X, 'mask': np.ones_like(X)}
-        y = {'data': y.reshape(-1, 1)}
-        X_val = {'data': X_val, 'mask': np.ones_like(X_val)}
-        y_val = {'data': y_val.reshape(-1, 1)}
+        X = {"data": X, "mask": np.ones_like(X)}
+        y = {"data": y.reshape(-1, 1)}
+        X_val = {"data": X_val, "mask": np.ones_like(X_val)}
+        y_val = {"data": y_val.reshape(-1, 1)}
 
         train_ds = DataSetCatCon(X, y, self.args.cat_idx, self.args.objective)
-        trainloader = DataLoader(train_ds, batch_size=self.batch_size, shuffle=True, num_workers=4)
+        trainloader = DataLoader(
+            train_ds, batch_size=self.batch_size, shuffle=True, num_workers=4
+        )
 
         val_ds = DataSetCatCon(X_val, y_val, self.args.cat_idx, self.args.objective)
-        valloader = DataLoader(val_ds, batch_size=self.args.val_batch_size, shuffle=True, num_workers=4)
+        valloader = DataLoader(
+            val_ds, batch_size=self.args.val_batch_size, shuffle=True, num_workers=4
+        )
 
         min_val_loss = float("inf")
         min_val_loss_idx = 0
@@ -110,7 +115,9 @@ class SAINT(BaseModelTorch):
                 cat_mask, con_mask = cat_mask.to(self.device), con_mask.to(self.device)
 
                 # We are converting the data to embeddings in the next step
-                _, x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask, self.model)
+                _, x_categ_enc, x_cont_enc = embed_data_mask(
+                    x_categ, x_cont, cat_mask, con_mask, self.model
+                )
 
                 reps = self.model.transformer(x_categ_enc, x_cont_enc)
 
@@ -144,9 +151,13 @@ class SAINT(BaseModelTorch):
                     x_categ, x_cont, y_gts, cat_mask, con_mask = data
 
                     x_categ, x_cont = x_categ.to(self.device), x_cont.to(self.device)
-                    cat_mask, con_mask = cat_mask.to(self.device), con_mask.to(self.device)
+                    cat_mask, con_mask = cat_mask.to(self.device), con_mask.to(
+                        self.device
+                    )
 
-                    _, x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask, self.model)
+                    _, x_categ_enc, x_cont_enc = embed_data_mask(
+                        x_categ, x_cont, cat_mask, con_mask, self.model
+                    )
                     reps = self.model.transformer(x_categ_enc, x_cont_enc)
                     y_reps = reps[:, 0, :]
                     y_outs = self.model.mlpfory(y_reps)
@@ -171,10 +182,14 @@ class SAINT(BaseModelTorch):
                 min_val_loss_idx = epoch
 
                 # Save the currently best model
-                self.save_model(filename_extension="best", directory="tmp")
+                # tabzilla: don't save the model...
+                # self.save_model(filename_extension="best", directory="tmp")
 
             if min_val_loss_idx + self.args.early_stopping_rounds < epoch:
-                print("Validation loss has not improved for %d steps!" % self.args.early_stopping_rounds)
+                print(
+                    "Validation loss has not improved for %d steps!"
+                    % self.args.early_stopping_rounds
+                )
                 print("Early stopping applies.")
                 break
 
@@ -182,11 +197,13 @@ class SAINT(BaseModelTorch):
         return loss_history, val_loss_history
 
     def predict_helper(self, X):
-        X = {'data': X, 'mask': np.ones_like(X)}
-        y = {'data': np.ones((X['data'].shape[0], 1))}
+        X = {"data": X, "mask": np.ones_like(X)}
+        y = {"data": np.ones((X["data"].shape[0], 1))}
 
         test_ds = DataSetCatCon(X, y, self.args.cat_idx, self.args.objective)
-        testloader = DataLoader(test_ds, batch_size=self.args.val_batch_size, shuffle=False, num_workers=4)
+        testloader = DataLoader(
+            test_ds, batch_size=self.args.val_batch_size, shuffle=False, num_workers=4
+        )
 
         self.model.eval()
 
@@ -199,7 +216,9 @@ class SAINT(BaseModelTorch):
                 x_categ, x_cont = x_categ.to(self.device), x_cont.to(self.device)
                 cat_mask, con_mask = cat_mask.to(self.device), con_mask.to(self.device)
 
-                _, x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask, self.model)
+                _, x_categ_enc, x_cont_enc = embed_data_mask(
+                    x_categ, x_cont, cat_mask, con_mask, self.model
+                )
                 reps = self.model.transformer(x_categ_enc, x_cont_enc)
                 y_reps = reps[:, 0, :]
                 y_outs = self.model.mlpfory(y_reps)
@@ -213,20 +232,22 @@ class SAINT(BaseModelTorch):
         return np.concatenate(predictions)
 
     def attribute(self, X, y, strategy=""):
-        """ Generate feature attributions for the model input.
-            Two strategies are supported: default ("") or "diag". The default strategie takes the sum
-            over a column of the attention map, while "diag" returns only the diagonal (feature attention to itself)
-            of the attention map.
-            return array with the same shape as X.
+        """Generate feature attributions for the model input.
+        Two strategies are supported: default ("") or "diag". The default strategie takes the sum
+        over a column of the attention map, while "diag" returns only the diagonal (feature attention to itself)
+        of the attention map.
+        return array with the same shape as X.
         """
         global my_attention
         # self.load_model(filename_extension="best", directory="tmp")
 
-        X = {'data': X, 'mask': np.ones_like(X)}
-        y = {'data': np.ones((X['data'].shape[0], 1))}
+        X = {"data": X, "mask": np.ones_like(X)}
+        y = {"data": np.ones((X["data"].shape[0], 1))}
 
         test_ds = DataSetCatCon(X, y, self.args.cat_idx, self.args.objective)
-        testloader = DataLoader(test_ds, batch_size=self.args.val_batch_size, shuffle=False, num_workers=4)
+        testloader = DataLoader(
+            test_ds, batch_size=self.args.val_batch_size, shuffle=False, num_workers=4
+        )
 
         self.model.eval()
         # print(self.model)
@@ -239,12 +260,16 @@ class SAINT(BaseModelTorch):
             """ an hook to extract the attention maps. """
             h = layer.heads
             q, k, v = layer.to_qkv(minput[0]).chunk(3, dim=-1)
-            q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), (q, k, v))
-            sim = einsum('b h i d, b h j d -> b h i j', q, k) * layer.scale
+            q, k, v = map(
+                lambda t: rearrange(t, "b n (h d) -> b h n d", h=h), (q, k, v)
+            )
+            sim = einsum("b h i d, b h j d -> b h i j", q, k) * layer.scale
             my_attention = sim.softmax(dim=-1)
 
         # print(type(self.model.transformer.layers[0][0].fn.fn))
-        self.model.transformer.layers[0][0].fn.fn.register_forward_hook(sample_attribution)
+        self.model.transformer.layers[0][0].fn.fn.register_forward_hook(
+            sample_attribution
+        )
         attributions = []
         with torch.no_grad():
             for data in testloader:
@@ -253,12 +278,16 @@ class SAINT(BaseModelTorch):
                 x_categ, x_cont = x_categ.to(self.device), x_cont.to(self.device)
                 cat_mask, con_mask = cat_mask.to(self.device), con_mask.to(self.device)
                 # print(x_categ.shape, x_cont.shape)
-                _, x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask, self.model)
+                _, x_categ_enc, x_cont_enc = embed_data_mask(
+                    x_categ, x_cont, cat_mask, con_mask, self.model
+                )
                 reps = self.model.transformer(x_categ_enc, x_cont_enc)
                 # y_reps = reps[:, 0, :]
                 # y_outs = self.model.mlpfory(y_reps)
                 if strategy == "diag":
-                    attributions.append(my_attention.sum(dim=1)[:, 1:, 1:].diagonal(0, 1, 2))
+                    attributions.append(
+                        my_attention.sum(dim=1)[:, 1:, 1:].diagonal(0, 1, 2)
+                    )
                 else:
                     attributions.append(my_attention.sum(dim=1)[:, 1:, 1:].sum(dim=1))
 
@@ -271,6 +300,8 @@ class SAINT(BaseModelTorch):
             "dim": trial.suggest_categorical("dim", [32, 64, 128, 256]),
             "depth": trial.suggest_categorical("depth", [1, 2, 3, 6, 12]),
             "heads": trial.suggest_categorical("heads", [2, 4, 8]),
-            "dropout": trial.suggest_categorical("dropout", [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]),
+            "dropout": trial.suggest_categorical(
+                "dropout", [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+            ),
         }
         return params
