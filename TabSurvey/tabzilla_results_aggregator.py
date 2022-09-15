8cd5f333379efe7b169d3572397a43187698665e
Jonathan Valverde <valverde@umd.edu>
2022-09-15 15:54:48 -0700

    Parallelizing results aggregation script.

--- TabSurvey/tabzilla_results_aggregator.py
+++ TabSurvey/tabzilla_results_aggregator.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import json
 from zipfile import ZipFile
 from tqdm import tqdm
+import multiprocessing
+import itertools
 
 import pandas as pd
 from google.cloud import storage
@@ -35,41 +37,55 @@ def download_results():
     print("Finished!")
 
 
+def aggregate_results_helper(results_file):
+    print(results_file)
+
+    result_list = []
+    dataset_name = results_file.parent.parent.parent.name
+    alg_name = results_file.parent.parent.name
+    exp_name = results_file.parent.name
+    result_set = results_file.name
+
+    with open(results_file, "r") as f:
+        contents = json.load(f)
+
+    for fold_number in range(len(contents["splits"])):
+        fold_results = dict(
+            dataset_fold_id=f"{dataset_name}__fold_{fold_number}",
+            dataset_name=dataset_name,
+            alg_name=contents["model"]["name"],
+            hparam_source=contents["hparam_source"],
+            trial_number=contents["trial_number"],
+            alg_hparam_id=f'{alg_name}__seed_{contents["experiemnt_args"]["hparam_seed"]}__trial_{contents["trial_number"]}',
+            exp_name=exp_name
+        )
+
+        for phase in ["train", "val", "test", "train-eval"]:
+            fold_results[f"time__{phase}"] = contents["timers"][phase][fold_number]
+            if phase == "train-eval":
+                continue
+            for metric in contents["scorers"][phase].keys():
+                fold_results[f"{metric}__{phase}"] = contents["scorers"][phase][metric][fold_number]
+
+        result_list.append(fold_results)
+    return result_list
+
 def aggregate_results():
-    consolidated_results = []
+    print("Parsing files...")
 
-    for results_file in tqdm(local_results_folder.glob("*/*/*/*.json")):
-        dataset_name = results_file.parent.parent.parent.name
-        alg_name = results_file.parent.parent.name
-        exp_name = results_file.parent.name
-        result_set = results_file.name
+    results_files = local_results_folder.glob("*/*/*/*.json")
+    # results_files = local_results_folder.glob("*/*/*/*_results.json") # Future versions will use this format
 
-        with open(results_file, "r") as f:
-            contents = json.load(f)
+    with multiprocessing.Pool() as pool:
+        consolidated_results = pool.map(aggregate_results_helper, results_files)
 
-        for fold_number in range(len(contents["splits"])):
-            fold_results = dict(
-                dataset_fold_id=f"{dataset_name}__fold_{fold_number}",
-                dataset_name=dataset_name,
-                alg_name=contents["model"]["name"],
-                hparam_source=contents["hparam_source"],
-                trial_number=contents["trial_number"],
-                alg_hparam_id=f'{alg_name}__seed_{contents["experiemnt_args"]["hparam_seed"]}__trial_{contents["trial_number"]}',
-                exp_name=exp_name
-            )
+    # Flatten list of lists
+    consolidated_results = list(itertools.chain(*consolidated_results))
 
-            for phase in ["train", "val", "test", "train-eval"]:
-                fold_results[f"time__{phase}"] = contents["timers"][phase][fold_number]
-                if phase == "train-eval":
-                    continue
-                for metric in contents["scorers"][phase].keys():
-                    fold_results[f"{metric}__{phase}"] = contents["scorers"][phase][metric][fold_number]
-
-            consolidated_results.append(fold_results)
-
+    print("Parsing done. Aggregating results...")
     consolidated_results = pd.DataFrame(consolidated_results)
-
-    consolidated_results.to_csv("metadataset.csv")
+    consolidated_results.sort_values(["dataset_fold_id", "alg_hparam_id"], inplace=True)
+    consolidated_results.to_csv("metadataset.csv", index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Aggregator for TabZilla results.")
